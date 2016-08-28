5df6c98e3eadb671e3faa2741a5d710e4afd1ec6
KarolIvette <karolcedeno95@gmail.com>
2016-08-28 12:32:25 -0500

    DOCUMENTACION DE TEXTO

---
+++ test_comment/src/principal/main.java
@@ -0,0 +1,17 @@
+package principal;
+/*
+ * @author: Karol Cedeño
+ * @version: 1.0
+ */
+
+public class main {
+	/*
+	 * @param: (int a, int b)
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
