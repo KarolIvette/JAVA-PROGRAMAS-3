ed4ca48af7b2e6e8ace41227aa50d53ed8cd4a14
KarolIvette <karolcedeno95@gmail.com>
2016-08-28 13:11:32 -0500

    PORCENTAJE DE CANTIDADES

---
+++ lambda/src/lambda/Recursive.java
@@ -0,0 +1,15 @@
+package lambda;
+
+public class Recursive{
+	int num1;
+	int num2;
+	
+	float porc = ((num2*100)/num1);
+	
+    (num1, num2) -> ((num2*100)/num1) == porc;
+     
+     
+     System.out.println(porc);
+}
+}
+
---
+++ lambda/src/lambda/main.java
@@ -0,0 +1,19 @@
+package lambda;
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		int num2;
+		
+		num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESE EL PRIMER NUMERO:"));
+		num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESE EL SEGUNDO NUMERO:"));
+		
+		
+		
+		
+	}
+
+}
